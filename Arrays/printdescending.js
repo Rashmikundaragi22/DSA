2d0d08c544c1c7018dc7ed432119630c3a346264
Rashmikundaragi22 <rashmikundaragi7305@gmail.com>
2026-01-26 14:54:10 +0530

    folder names changed

--- Arrays/printdescending.js
+++
@@ -1,7 +0,0 @@
-function printascending(){
-    let char = [1,7,8,3,31,8,10,3];
-    char.sort((a,b)=>b-a)
-    console.log("Ascending order",char);
-    return char;
-}
-console.log(printascending());
--- Arrays/secondLargestDigit.js
+++
@@ -1,8 +0,0 @@
-function secondLargest(str){
-    let digits = str.split('').filter(char => !isNaN(char)).map(Number);
-    let unicharset = [...new Set(digits)]
-    console.log("unique didgits are",unicharset)
-    unicharset.sort((a,b)=>b-a);
-    return unicharset.length >=2 ? unicharset[1] : -1;
-}
-console.log(secondLargest("v4vwv2b4r3h68djs9"))
